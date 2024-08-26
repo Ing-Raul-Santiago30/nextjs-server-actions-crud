c3e1733394b9b5d2c4c93103708ba691b12ac071
DESKTOP-H2SMBA5\Ing Raul Santiago <ing.raul.santiago30@gmail.com>
2024-08-26 17:48:21 -0400

    subir cambio

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import { Taskform } from './Task-form'
-
+import * as React from "react";
+import { Taskform } from "./Task-form";
+import { Task } from "@prisma/client";
 
 function NewPage() {
+  // Puedes definir un objeto `task` con valores predeterminados
+  const task: Task = {
+    id: 0, // o cualquier ID por defecto
+    name: '',
+    description: '',
+    priority: 'low',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
   return (
-    <div className='flex justify-center items-center h-screen'>
-     <Taskform/>
+    <div className="flex justify-center items-center h-screen">
+      <Taskform task={task} />
     </div>
-  )
+  );
 }
 
-export default NewPage
+export default NewPage;
